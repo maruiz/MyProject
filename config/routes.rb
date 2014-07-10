7ab31e58a3167047d7c8fdbd395f45982a80c7be
vagrant <vagrant@precise32.(none)>
2014-07-10 02:35:30 +0000

    scaffold student & admin, subtypes for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+
+  resources :admins
+
+  resources :students
+
   devise_for :users
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+
  root 'welcome#index'
 
   # Example of regular route:
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140708015145) do
+ActiveRecord::Schema.define(version: 20140710023021) do
+
+  create_table "admins", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
